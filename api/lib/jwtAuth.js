721d69079dedf814749e3050a1268700451df3c6
Kyle Warbis <kyle@Kyles-MacBook-Pro.local>
2014-10-09 16:56:17 -0700

    add jwt passport etc.

---
+++ api/lib/jwtAuth.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var User = require('../models/user.js');
+var jwt = require('jwt-simple');
+
+module.exports = function(app){
+	var jwtauth = {};
+
+	jwtauth.auth = function(req, res, next){
+		var token = req.headers.jwt_token;
+
+		if(token){
+			try{
+				var decoded = jwt.decode(token, app.get('jwtTokenSecret'));
+
+				User.findOne({'_id': decoded.iss}. function(err, user){
+					if(err){return res.send(500, err);}
+
+					if(new Date(decoded.expires).getTime() < new Date().getTime()){
+						{
+						return res.send(401, {'msg': 'expired token'});
+						}
+					};
+
+					//perform if token accepted
+					req.user = user;
+					return next();
+				});
+			}catch(err){
+				return res.send(500);
+			}else {
+				return res.send(401, {'msg': 'no token found'});
+			}
+		}
+		
+	};
+	return jwtauth;
+}
--- server.js
+++ server.js
@@ -2,25 +2,34 @@ var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
-
-mongoose.connect('mongodb://localhost/users');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
 
 var app = express();
+mongoose.connect('mongodb://localhost/users');
 
+var jwtauth = require('./api/lib/jwtAuth')(app);
+app.set('jwtTokenSecret', process.env.JWT_SECRET || 'Changeme');
+
+//JWT AUTH
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
 
-app.set('port', process.env.PORT || 3000);
+//PASSPORT
+app.use(passport.initialize());
+require('./api/lib/passport')
+
 
 //=====API Routes=====
-require('./api/routes/createUser')(app); //User creater
+require('./api/routes/createUser')(app, passport, jwtauth.auth); //User creater
 require('./api/routes/allUsers')(app); //Shows all users
 require('./api/routes/deleteUser')(app); // delete user
-require('./api/routes/editUser')(app);  //edit user
+require('./api/routes/editUser')(app, jwtauth.auth);  //edit user
 require('./api/routes/viewUser')(app); //show single user
 
 var server = http.createServer(app);
+
+app.set('port', process.env.PORT || 3000);
 
 server.listen(app.get('port'), function () {
 	console.log('Server start on:' + app.get('port'));
